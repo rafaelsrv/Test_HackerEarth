d613aadd509dc7e6ce1801a2e385ac98013b59b8
RafaelSRV <rafael-srv@hotmail.com>
2024-01-04 10:02:54 -0300

    Renderizando nomes e seus usernames

--- Teste_HackerEarth/src/App.jsx
+++ Teste_HackerEarth/src/App.jsx
@@ -1,23 +1,40 @@
 
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
   const url = 'https://jsonplaceholder.typicode.com/users'
   
-  let ids = []
+  
+  const [clientData, setClientData] = useState([])
+  let clientNames = []
 
-  async function Search (){
+  
    
-   await fetch(url).then(resp => resp.json()).then(
-    names => names.map((data)=> {ids.push(data.name)}))
-    console.log(ids)
+  async function Search (){
+
+    await fetch(url).then(resp => resp.json()).then(data =>
+      setClientData(data))
+      
+
+  }
+
+  useEffect(()=>{
+    Search()
+
+  },[])
   
-  }  
-  
-  Search()
   
   return (
-    <div className="container"><p>Hello World</p></div>
+    <div className="container">
+      
+        <ul>
+          
+          {clientData.map(client=><li>{client.name} - Username: {client.username}</li>)}
+
+        </ul>
+      
+    </div>
   )
 }
 
