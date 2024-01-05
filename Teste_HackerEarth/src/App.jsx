51efc03a8c7ddf5ae076a34df7f74329cb1829cc
RafaelSRV <rafael-srv@hotmail.com>
2024-01-05 13:01:02 -0300

    Projeto Finalizado

--- Teste_HackerEarth/src/App.jsx
+++ Teste_HackerEarth/src/App.jsx
@@ -37,17 +37,18 @@ function App() {
         <div className='tables'>    
           <div >
             <h1 style={{textAlign: 'center'}}>Name</h1>
-            <table className="name">{clientData.map(client=><tr key ={client.id}>{(client.name.toLowerCase()).includes(searchData)? (client.name) : <tr></tr>}</tr>)}</table>
+            <table className="name">{clientData.map(client=><tr key ={client.id}>{(client.name.toLowerCase()).includes(searchData)? (client.name) : <></>}</tr>)}</table>
           </div>
 
           <div>
             <h1 style={{textAlign: 'center'}}>Username</h1>
-            <table className="username">{clientData.map(client=><tr key = {client.id}> {(client.username.toLowerCase()).includes(searchData)? (client.username) : <tr></tr>} </tr>)}</table>
+            <table className="username">{clientData.map(client=><tr key = {client.id}> {(client.username.toLowerCase()).includes(searchData)? (client.username) : <></>} </tr>)}</table>
           </div>
           
           <div>
             <h1 style={{textAlign: 'center'}}>Email</h1>
-            <table className="email">{clientData.map(client=><tr key ={client.id}>{(client.email.toLowerCase()).includes(searchData)? (client.email): <tr></tr>}</tr>)}</table>
+            <table className="email">{clientData.map(client=><tr key ={client.id}>{(client.email.toLowerCase()).includes(searchData)? (client.email): <></>}</tr>)}</table>
+            
           </div>
         </div> 
         <input type="text" className='searchInput' placeholder='Buscar...'  onChange={((e)=>setSearchData((e.target.value).toLowerCase()))} />
