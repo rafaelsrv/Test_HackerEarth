a613c6ddb8e0442d3549b4a30e5e9e1da2c64298
RafaelSRV <rafael-srv@hotmail.com>
2024-01-05 12:55:35 -0300

    Implementando buscas

--- Teste_HackerEarth/src/App.jsx
+++ Teste_HackerEarth/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   
   
   const [clientData, setClientData] = useState([])
-  let clientNames = []
+  const [searchData, setSearchData] = useState('')
+
+ 
 
   
    
@@ -23,19 +25,38 @@ function App() {
     Search()
 
   },[])
+  console.log(searchData)
+ 
+
+
   
   
   return (
     <div className="container">
       
-        <ul>
-          
-          {clientData.map(client=><li>{client.name} - Username: {client.username}</li>)}
+        <div className='tables'>    
+          <div >
+            <h1 style={{textAlign: 'center'}}>Name</h1>
+            <table className="name">{clientData.map(client=><tr key ={client.id}>{(client.name.toLowerCase()).includes(searchData)? (client.name) : <tr></tr>}</tr>)}</table>
+          </div>
 
-        </ul>
-      
+          <div>
+            <h1 style={{textAlign: 'center'}}>Username</h1>
+            <table className="username">{clientData.map(client=><tr key = {client.id}> {(client.username.toLowerCase()).includes(searchData)? (client.username) : <tr></tr>} </tr>)}</table>
+          </div>
+          
+          <div>
+            <h1 style={{textAlign: 'center'}}>Email</h1>
+            <table className="email">{clientData.map(client=><tr key ={client.id}>{(client.email.toLowerCase()).includes(searchData)? (client.email): <tr></tr>}</tr>)}</table>
+          </div>
+        </div> 
+        <input type="text" className='searchInput' placeholder='Buscar...'  onChange={((e)=>setSearchData((e.target.value).toLowerCase()))} />
     </div>
+
+
   )
 }
 
 export default App
+//<div className="username">{clientData.map(client=><li>{client.username}</li>)}</div>
+//<div className="email">{clientData.map(client=><li>{client.email}</li>)}</div>
